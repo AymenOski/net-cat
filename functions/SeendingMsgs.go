fbcf433d2df45c464cec7948be526347b91936e6
AymenOski <aymenazizi610@gmail.com>
2025-02-27 18:57:39 +0000

    Adding some comments

--- functions/SeendingMsgs.go
+++ functions/SeendingMsgs.go
@@ -10,6 +10,7 @@ import (
 
 func SendingMsgs(sender net.Conn) {
 	for {
+		// Reads and listen for messages
 		msg, err := bufio.NewReader(sender).ReadString('\n')
 		if err != nil {
 			tempName := Clients[sender]
@@ -23,10 +24,12 @@ func SendingMsgs(sender net.Conn) {
 			msg = strings.ReplaceAll(msg, "\r\n", "")
 			msg = strings.ReplaceAll(msg, "\n", "")
 			for Client := range Clients {
+				// send the msg to all clients except the sender
 				if Client != sender {
 					Client.Write([]byte(fmt.Sprintf("\n[%s] [%s] : %s", time.Now().Format("2006-01-02 15:04:05"), Clients[sender], msg)))
 					Client.Write([]byte(fmt.Sprintf("\n[%s] [%s] : %s", time.Now().Format("2006-01-02 15:04:05"), Clients[Client], "")))
 				} else {
+					// send this msg to the sender
 					Client.Write([]byte(fmt.Sprintf("[%s] [%s] : %s", time.Now().Format("2006-01-02 15:04:05"), Clients[sender], "")))
 				}
 			}
